43141995be78093a700dc9ae80a4ace885e617ca
krysmathis <krysmathis@gmail.com>
2018-03-05 09:46:25 -0600

    added issue_template.md

---
+++ Controllers/FacilityController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LodestarHealthDataApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LodestarHealthDataApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class FacilityController : Controller
+    {
+        private readonly LodestarAPIContext _context;
+
+        public FacilityController (LodestarAPIContext context) {
+            _context = context;
+        }
+        // GET api/values/5
+        [HttpGet]
+        public IEnumerable<string> Get(string latitude, string longitude)
+        {
+            return new string[] { latitude, longitude };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(string system)
+        {
+            return "value";
+        }
+
+    }
+}
